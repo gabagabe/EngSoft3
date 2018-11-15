7f20ba6fbbe819eb391546abdccdfcdc16a8b7da
gabagabe <44407912+gabagabe@users.noreply.github.com>
2018-11-15 21:00:36 +0000

    Delete display_timer.d

--- Lights2/Debug/src/display_timer.d
+++
@@ -1,9 +0,0 @@
-src/display_timer.o src/display_timer.d: ../src/display_timer.c \
- ../src/display_timer.h ../src/variables.h \
- ../../Lights2_bsp/microblaze_0/include/xil_types.h
-
-../src/display_timer.h:
-
-../src/variables.h:
-
-../../Lights2_bsp/microblaze_0/include/xil_types.h:
